645a545a38cfa3a08129d255358282ec4bd8b30f
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-04-23 20:59:50 -0600

    Array literals

--- 5 - Arrays & Sets/Tokyo.swift
+++ 5 - Arrays & Sets/Tokyo.swift
@@ -4,3 +4,10 @@ var subwayChild = [Int]()
 
 print(subwayAdult)
 print(subwayChild)
+
+//  Array Literals
+var subwayAdult = [800, 1200, 1500]
+var subwayChild: [Int] = [400, 600, 750]
+
+print(subwayAdult)
+print(subwayChild)
