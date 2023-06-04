93e6b4bd09a48c340846103a288f7ebafae70561
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-06-04 11:57:29 -0600

    Mutating Methods

--- 8 - Structures/Band.swift
+++ 8 - Structures/Band.swift
@@ -1,26 +1,32 @@
-// Add your code below 🎹 🎸 🥁 
 struct Band {
-  var genre: String
-  var members: Int
-  var isActive: Bool
-  
-  init(genre: String, members: Int, isActive: Bool) {
-    self.genre = genre
-    self.members = members
-    self.isActive = isActive
-  }
-  
-  // Add your method below 🤘
-  func pumpUpCrowd() -> String {
-    if self.isActive == true {
-      return "Are you ready to ROCK?"
-    } else {
-      return "..."
+    var genre: String
+    var members: Int
+    var isActive: Bool
+
+    init(genre: String, members: Int, isActive: Bool) {
+        self.genre = genre
+        self.members = members
+        self.isActive = isActive
     }
-  }
+
+    func pumpUpCrowd() -> String {
+        if self.isActive == true {
+            return "Are you ready to ROCK?"
+        } else {
+            return "..."
+        }
+    }
+
+    mutating func changeGenre(newGenre: String) -> String {
+        self.genre = newGenre
+        return self.genre
+    }
 }
 
 var maroon5 = Band(genre: "pop", members: 5, isActive: true)
 var fooFighters = Band(genre: "rock", members: 6, isActive: true)
+var journey = Band(genre: "jazz", members: 5, isActive: true)
+var bandsNewGenre = journey.changeGenre(newGenre: "rock")
 
 print(fooFighters.pumpUpCrowd())
+print(bandsNewGenre)
