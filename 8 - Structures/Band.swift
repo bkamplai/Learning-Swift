65179241dae5c09618b2eedf08e50573f51b15d6
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-06-03 21:54:15 -0600

    Structure Methods

--- 8 - Structures/Band.swift
+++ 8 - Structures/Band.swift
@@ -3,6 +3,24 @@ struct Band {
   var genre: String
   var members: Int
   var isActive: Bool
+  
+  init(genre: String, members: Int, isActive: Bool) {
+    self.genre = genre
+    self.members = members
+    self.isActive = isActive
+  }
+  
+  // Add your method below 🤘
+  func pumpUpCrowd() -> String {
+    if self.isActive == true {
+      return "Are you ready to ROCK?"
+    } else {
+      return "..."
+    }
+  }
 }
 
 var maroon5 = Band(genre: "pop", members: 5, isActive: true)
+var fooFighters = Band(genre: "rock", members: 6, isActive: true)
+
+print(fooFighters.pumpUpCrowd())
