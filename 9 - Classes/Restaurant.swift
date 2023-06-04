03b71b4b583bb63b5aa6629bcb6c88974af92289
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-06-04 13:47:21 -0600

    Creating an Instance

--- 9 - Classes/Restaurant.swift
+++ 9 - Classes/Restaurant.swift
@@ -4,3 +4,15 @@ class Restaurant {
     var rating = 0.0
     var delivery = false
 }
+
+var bobsBurgers = Restaurant()
+
+bobsBurgers.name = "Bob's Burgers"
+bobsBurgers.type = ["Burgers", "Fast Food"]
+bobsBurgers.rating = 3.5
+bobsBurgers.delivery = true
+
+print(bobsBurgers.name)
+print(bobsBurgers.type)
+print(bobsBurgers.rating)
+print(bobsBurgers.delivery)
