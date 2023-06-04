d9f711b15b69b4209a095084c61a01b50b88aa8f
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-06-04 13:56:44 -0600

    Init Method

--- 9 - Classes/Restaurant.swift
+++ 9 - Classes/Restaurant.swift
@@ -3,11 +3,19 @@ class Restaurant {
     var type = [""]
     var rating = 0.0
     var delivery = false
+
+    init(name: String, type: [String], rating: Double, delivery: Bool) {
+        self.name = name
+        self.type = type
+        self.rating = rating
+        self.delivery = delivery
+  }
 }
 
-var bobsBurgers = Restaurant()
+// var bobsBurgers = Restaurant()
+var laRatatouille = Restaurant(name: "La Ratatouille", type: ["French"], rating: 4.7, delivery: false)
 
-bobsBurgers.name = "Bob's Burgers"
+/* bobsBurgers.name = "Bob's Burgers"
 bobsBurgers.type = ["Burgers", "Fast Food"]
 bobsBurgers.rating = 3.5
 bobsBurgers.delivery = true
@@ -15,4 +23,9 @@ bobsBurgers.delivery = true
 print(bobsBurgers.name)
 print(bobsBurgers.type)
 print(bobsBurgers.rating)
-print(bobsBurgers.delivery)
+print(bobsBurgers.delivery) */
+
+print(laRatatouille.name)
+print(laRatatouille.type)
+print(laRatatouille.rating)
+print(laRatatouille.delivery)
