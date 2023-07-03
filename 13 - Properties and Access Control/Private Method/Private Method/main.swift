83c14690c0e41a41367ef173e901e4f45fbd333a
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-07-03 15:25:27 -0600

    Defining a Private Method

--- 13 - Properties and Access Control/Private Method/Private Method/main.swift
+++ 13 - Properties and Access Control/Private Method/Private Method/main.swift
@@ -5,7 +5,24 @@
 //  Created by Brandon Kamplain on 7/3/23.
 //
 
-import Foundation
+struct Office {
+    let paperclipCost = 10
+    private var paperclipSales: Int
+    
+    init(paperclipSales: Int) {
+        self.paperclipSales = paperclipSales
+    }
+    
+    private func getSecretRevenue() -> Int {
+        return 100
+    }
+    
+    func printTotalRevenue() {
+        let totalRevenue = (paperclipSales * paperclipCost) + getSecretRevenue()
+        print("Our total revenue this month is $\(totalRevenue).")
+    }
+}
 
-print("Hello, World!")
-
+let alphaOffice = Office(paperclipSales: 18)
+alphaOffice.printTotalRevenue()
+//alphaOffice.getSecretRevenue()  // Error: 'getSecretRevenue' is inaccessible due to 'private' protection level
