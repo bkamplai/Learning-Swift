157cfb4c91a5e0e44ee93800f750ce4d946510e6
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-07-22 09:07:19 -0600

    Initial Commit

---
+++ Code History/Code History/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Brandon Kamplain on 7/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
