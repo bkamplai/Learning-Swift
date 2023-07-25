93defbe6db39d8eeecc8ab6420039fa6da604ba1
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-07-24 19:28:27 -0600

    Building the Final Screen

--- Code History/Code History/Views/GameView.swift
+++ Code History/Code History/Views/GameView.swift
@@ -26,6 +26,15 @@ struct GameView: View {
                 QuestionView(question: viewModel.currentQuestion)
             }
         }
+        .background(
+//          'init(destination:isActive:label:)' was deprecated in iOS 16.0: use
+//          NavigationLink(value:label:), or
+//          navigationDestination(isPresented:destination:), inside a
+//          NavigationStack or NavigationSplitView
+            NavigationLink(destination: ScoreView(viewModel: ScoreViewModel(correctGuesses: viewModel.correctGuesses, incorrectGuesses: viewModel.incorrectGuesses)),
+                           isActive: .constant(viewModel.gameIsOver),
+                           label: { EmptyView() })
+        )
         .foregroundColor(.white)
         .navigationBarHidden(true)
         .environmentObject(viewModel)
